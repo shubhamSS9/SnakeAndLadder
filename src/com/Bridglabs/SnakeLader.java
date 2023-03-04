6856a5c20128224bd8fa3dda25e3bf65a4039b70
shubhamSS9 <shubhamshinde0999@gmail.com>
2023-03-04 21:57:50 +0530

    added Snake And Lader

---
+++ src/com/Bridglabs/SnakeLader.java
@@ -0,0 +1,9 @@
+package com.Bridglabs;
+
+public class SnakeLader {
+    public static void main(String[] args) {
+
+        System.out.println("Start the Snake and Ladder Game");
+        System.out.println("Single Player at Start Position 0");
+    }
+}
