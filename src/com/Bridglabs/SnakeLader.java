d91b436740d51dee51e3c7229ee591008534a05f
shubhamSS9 <shubhamshinde0999@gmail.com>
2023-03-04 22:22:03 +0530

    added UC3_PlayersCheckForOption

--- src/com/Bridglabs/SnakeLader.java
+++ src/com/Bridglabs/SnakeLader.java
@@ -1,14 +1,33 @@
 package com.Bridglabs;
+import
+
+import java.util.Scanner;
 
 public class SnakeLader {
     public static void main(String[] args) {
-
         System.out.println("Start the Snake and Ladder Game");
-        System.out.println("Single Player at Start Position 0");
+        System.out.println("Player 1 at Start Position 0");
+        int RollDice = (int) (Math.random() * 6 + 1);
+        System.out.println("dice rolls by: " + RollDice);
+        Scanner InputValue = new Scanner(System.in);
+        System.out.println("enter Option");
+        int Option = InputValue.nextInt();
+        int position = 0;
+        switch (Option) {
+            case 1:
+                System.out.println("NO PLAY: " + position);
+                System.out.println("Player stays in the same position");
+                break;
+            case 2:
+                System.out.println("LADDER: " + (RollDice + position));
+                System.out.println("Player moves ahead by: " + RollDice);
+                break;
+            case 3:
+                System.out.println("SNAKE: " + (RollDice + position));
+                System.out.println("Player moves behind by: " + RollDice);
 
-            int RollDice=(int)(Math.random()*6+1);
-            System.out.println("Player 1 rolls the die and get: "+RollDice);
         }
     }
+}
 
 
